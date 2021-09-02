3ae4772f1c5010179ca4947a270691d6f68ece27
erfany78 <erfany59@gmail.com>
2021-09-02 20:28:56 +0430

    update commit

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import psutil
-import eventlet
-import socketio
 import requests
 import time
 # Calling psutil.cpu_precent() for 4 seconds
-if __name__ == "__main__":
+if _name=='main_':
     while True :
-        hdd = psutil.disk_usage('/')
-        url = "http://back.mkbedu.ir/dashboard/insert"
-        payload='server=adobe.mukaap.ir&cpu='+str(psutil.cpu_percent(4))+'&storageSize='+str(hdd.total)+'&storageUsage='+str(hdd.used)+'&storageFree=5&ram=2'
-        headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-        }
+        try:
+            hdd = psutil.disk_usage('/')
+            ram=psutil.virtual_memory()
+            url = "http://10.10.6.42:3030/dashboard/insert"
+            payload='server=adobe.mukaap.ir&cpu='+str(psutil.cpu_percent(4))+'&storageSize='+str(hdd.total)+'&storageUsage='+str(hdd.used)+'&storageFree=5&ram='+str(psutil.virtual_memory().percent)
+            headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+            }
 
-        response = requests.request("POST", url, headers=headers, data=payload)
-        time.sleep(120)
-        print(response.text)
+            response = requests.request("POST", url, headers=headers, data=payload)
+            time.sleep(1)
+            print(response.text)
+        except:
+            print('error')
